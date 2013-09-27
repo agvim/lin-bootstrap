26e75634f8f75116a30fbc7c5f7794151bcc7710
agvim <agvim@server.fake>
2013-09-27 13:28:21 +0200

    fixes

--- homeshick_bootstrap.sh
+++ homeshick_bootstrap.sh
@@ -2,30 +2,27 @@
 . "$(dirname $0)/lib/package_tests.sh"
 
 #get homeshick
-git clone git://github.com/agvim/homeshick.git $HOME/.homesick/repos/homeshick
+git clone https://github.com/agvim/homeshick.git $HOME/.homesick/repos/homeshick
 homeshick="yes | $HOME/.homesick/repos/homeshick/homeshick.sh"
 
 #clone the basic castles
-$homeshick clone agvim/cfg-bash
-$homeshick clone agvim/cfg-tmux
-$homeshick clone agvim/cfg-vim
+eval $homeshick clone agvim/cfg-bash
+eval $homeshick clone agvim/cfg-tmux
+eval $homeshick clone agvim/cfg-vim
 
 #if we have xfce4-terminal, clone the xfce4 castle
 is_installed xfce4-terminal
 if [[ $? -eq 1 ]]
 then
-    $homeshick clone agvim/cfg-xfce4-terminal
+    eval $homeshick clone agvim/cfg-xfce4-terminal
 fi
 
 #if we have xfce4 panel, clone the desktop castle
 is_installed xfce4-panel
 if [[ $? -eq 1 ]]
 then
-    $homeshick clone agvim/cfg-xfce4-desktop
+    eval $homeshick clone agvim/cfg-xfce4-desktop
 fi
 
-$homeshick link
+eval $homeshick link
 echo "Finished homesick configuration"
-
-#$HOME/.spf13-vim-agvim/bootstrap.sh
-#echo "Finished vim configuration"
