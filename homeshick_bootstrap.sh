1a916fe300db1fbe8cc909cd768417fe126b34c5
agvim <agvim@server.fake>
2013-10-24 10:46:25 +0200

    fixed bootstrap for github repositories

--- homeshick_bootstrap.sh
+++ homeshick_bootstrap.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 . "$(dirname $0)/lib/package_tests.sh"
 
+#cfg-xfce4-terminal and cfg-xfce4-desktop are not properly interpreted as github
+#paths
+github="https://github.com"
+
 #get homeshick
-git clone https://github.com/agvim/homeshick.git $HOME/.homesick/repos/homeshick
+git clone $github/agvim/homeshick.git $HOME/.homesick/repos/homeshick
 homeshick="yes | $HOME/.homesick/repos/homeshick/homeshick.sh"
 
 #clone the basic castles
-eval $homeshick clone agvim/cfg-bash
-eval $homeshick clone agvim/cfg-tmux
-eval $homeshick clone agvim/cfg-vim
+eval $homeshick clone $github/agvim/cfg-bash
+eval $homeshick clone $github/agvim/cfg-tmux
+eval $homeshick clone $github/agvim/cfg-vim
 
 #if we have xfce4-terminal, clone the xfce4 castle
 is_installed xfce4-terminal
 if [[ $? -eq 1 ]]
 then
-    eval $homeshick clone agvim/cfg-xfce4-terminal
+    eval $homeshick clone $github/agvim/cfg-xfce4-terminal
 fi
 
 #if we have xfce4 panel, clone the desktop castle
 is_installed xfce4-panel
 if [[ $? -eq 1 ]]
 then
-    eval $homeshick clone agvim/cfg-xfce4-desktop
+    eval $homeshick clone $github/agvim/cfg-xfce4-desktop
 fi
 
 eval $homeshick link
