00aebba52b4b10c4da80cebbfe21b25139147ff4
agvim <agvim@server.fake>
2014-07-10 16:18:22 +0200

    removed cfg-xfce4-desktop added zsh

--- homeshick_bootstrap.sh
+++ homeshick_bootstrap.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 . "$(dirname $0)/lib/package_tests.sh"
 
-#cfg-xfce4-terminal and cfg-xfce4-desktop are not properly interpreted as github
-#paths
+# cfg-xfce4-terminal and cfg-xfce4-desktop are not properly interpreted as
+# github paths
 github="https://github.com"
 
-#get homeshick
+# get homeshick
 git clone $github/agvim/homeshick.git $HOME/.homesick/repos/homeshick
 homeshick="yes | $HOME/.homesick/repos/homeshick/bin/homeshick"
 
-#clone the basic castles
+# clone the basic castles
 eval $homeshick clone $github/agvim/cfg-bash
 eval $homeshick clone $github/agvim/cfg-tmux
 eval $homeshick clone $github/agvim/cfg-vim
 
-#if we have xfce4-terminal, clone the xfce4 castle
+# if zsh is installed personalize it like bash
+is_installed zsh
+if [[ $? -eq 1 ]]
+then
+    eval $homeshick clone $github/agvim/cfg-zsh
+fi
+
+# if we have xfce4-terminal, clone the xfce4 castle
 is_installed xfce4-terminal
 if [[ $? -eq 1 ]]
 then
     eval $homeshick clone $github/agvim/cfg-xfce4-terminal
-fi
-
-#if we have xfce4 panel, clone the desktop castle
-is_installed xfce4-panel
-if [[ $? -eq 1 ]]
-then
-    eval $homeshick clone $github/agvim/cfg-xfce4-desktop
 fi
 
 eval $homeshick link
--- install_packages.sh
+++ install_packages.sh
@@ -10,32 +10,36 @@ else
     aptitude='sudo aptitude'
 fi
 
-#install aptitude first
+# install aptitude first
 $aptget update
 $aptget install -y aptitude
 
-#always useful packages
+# always useful packages
 PACKAGES="tmux vim"
 
-#stuff for vim in a desktop machine
+# stuff for vim in a desktop machine
 PACKAGES="$PACKAGES git exuberant-ctags silversearcher-ag"
-#stuff for YouCompleteMe vim plugin
-PACKAGES="$PACKAGES build-essential cmake python-dev"
 
-#throw in gvim if we have x11 installed
+# # stuff for YouCompleteMe vim plugin
+# PACKAGES="$PACKAGES build-essential cmake python-dev"
+
+# zsh shell
+PACKAGES="$PACKAGES zsh"
+
+# throw in gvim if we have x11 installed
 is_installed x11-common
 if [[ $? -eq 1 ]]
 then
     PACKAGES="$PACKAGES vim-gtk fonts-inconsolata"
 fi
 
-#if we have xfce4 panel, install the panel plugins and orage calendar
+# if we have xfce4 panel, install the panel plugins
 is_installed xfce4-panel
 if [[ $? -eq 1 ]]
 then
-    PACKAGES="$PACKAGES xfce4-systemload-plugin xfce4-netload-plugin xfce4-places-plugin orage"
+    PACKAGES="$PACKAGES xfce4-systemload-plugin xfce4-netload-plugin xfce4-places-plugin"
 fi
 
-#use aptitude to install the basic packages
+# use aptitude to install the basic packages
 $aptitude update
 $aptitude install -y $PACKAGES
